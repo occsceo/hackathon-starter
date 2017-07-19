1f157d1ccc374fbf883e83974e045e74c176fb1a
Matt Bernard <occsceo@gmail.com>
2017-07-19 17:28:45 -0400

    updates pkgs, fix lint issue

--- app.js
+++ app.js
@@ -106,8 +106,7 @@ app.use((req, res, next) => {
       !req.path.match(/^\/auth/) &&
       !req.path.match(/\./)) {
     req.session.returnTo = req.path;
-  } else if (req.user &&
-      req.path == '/account') {
+  } else if (req.user && req.path === '/account') {
     req.session.returnTo = req.path;
   }
   next();
